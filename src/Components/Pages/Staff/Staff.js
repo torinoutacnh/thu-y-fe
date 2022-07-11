fbcd361cf330a765a62cda48c5ebf53545ef080c
tori <torinoutacnh@gmail.com>
2022-07-11 23:25:40 +0700

    ád

--- src/Components/Pages/Staff/Staff.js
+++ src/Components/Pages/Staff/Staff.js
@@ -7,47 +7,47 @@ import { ApiRoute } from "Api/ApiRoute";
 import { useAuth } from "Modules/hooks/useAuth";
 const { Header, Footer, Sider, Content } = Layout;
 const Staff = () => {
-  const [data, setdata] = useState([]);
-  const [form, setForm] = useState();
-  const [page, setPage] = useState({
-    pageIndex: 1,
-    pageNumber: 100,
-  });
-  const user = useAuth();
+	const [data, setdata] = useState([]);
+	const [form, setForm] = useState();
+	const [page, setPage] = useState({
+		pageIndex: 1,
+		pageNumber: 100,
+	});
+	const user = useAuth();
 
-  useEffect(() => {
-    if (user?.token) {
-      fetch(
-        process.env.REACT_APP_API.concat(ApiRoute.getUser, "?") +
-          new URLSearchParams(page),
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer ".concat(user.token),
-          },
-        }
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setdata(data.data);
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [user.token, data]);
+	useEffect(() => {
+		if (user?.token) {
+			fetch(
+				process.env.REACT_APP_API.concat(ApiRoute.getUser, "?") +
+					new URLSearchParams(page),
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer ".concat(user.token),
+					},
+				}
+			)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					setdata(data.data);
+				})
+				.catch((error) => console.log(error));
+		}
+	}, [user.token]);
 
-  return (
-    <div className="staff-page">
-      <Layout className="nav-staff">
-        <Layout className="content-staff">
-          <Content>
-            <ContentStaff liststaff={data} />
-          </Content>
-        </Layout>
-      </Layout>
-    </div>
-  );
+	return (
+		<div className="staff-page">
+			<Layout className="nav-staff">
+				<Layout className="content-staff">
+					<Content>
+						<ContentStaff liststaff={data} />
+					</Content>
+				</Layout>
+			</Layout>
+		</div>
+	);
 };
 
 export default Staff;
---
+++ src/Utils/Validate.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ValidateEmail(email: string) {
+
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(email)?.toLowerCase());
+
+}
+
+export { ValidateEmail };

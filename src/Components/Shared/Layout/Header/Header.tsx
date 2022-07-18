8be176e9e08a35c0f73ba6496afa9af5b140ca7e
kioliki <torinoutacnh@gmail.com>
2022-07-18 15:47:33 +0700

    y

--- src/Components/Shared/Layout/Header/Header.tsx
+++ src/Components/Shared/Layout/Header/Header.tsx
@@ -3,42 +3,84 @@ import { abattoirEndpoints } from "Components/router/AbattoirRoutes";
 import { RouteEndpoints } from "Components/router/MainRouter";
 import { quarantineEndpoints } from "Components/router/QuarantineRoutes";
 import { staffEndpoints } from "Components/router/StaffRoutes";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./header.css";
 import "./nav.css";
 
 const Header = () => {
   const navRef = useRef(null);
 
+  useEffect(() => {
+    const menu = document.querySelector("#menu-responsive") as HTMLElement;
+    const navbar = document.querySelector(".nav-bar-top");
+    menu.onclick = () => {
+      menu.classList.toggle("fa-times");
+      navbar.classList.toggle("active");
+    };
+  });
+
+  const [showItems1, setShowItems1] = useState(false);
+  const [iconRotate1, setIconRotate1] = useState(false);
+  function handleClicElements1() {
+    setShowItems1(!showItems1);
+    setIconRotate1(!iconRotate1);
+  }
+
+  const [showItems2, setShowItems2] = useState(false);
+  const [iconRotate2, setIconRotate2] = useState(false);
+  function handleClicElements2() {
+    setShowItems2(!showItems2);
+    setIconRotate2(!iconRotate2);
+  }
+
+  const [showItems3, setShowItems3] = useState(false);
+  const [iconRotate3, setIconRotate3] = useState(false);
+  function handleClicElements3() {
+    setShowItems3(!showItems3);
+    setIconRotate3(!iconRotate3);
+  }
+
   return (
     <>
       <PageHeader>
-        <div className="header_menu">
-          <img
-            src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
-            alt="logo"
-            width={30}
-          />
-          <h4>PHẦN MỀM QUẢN LÝ KIỂM SOÁT GIẾT MỔ VÀ KIỂM DỊCH</h4>
-          <div className="btn-nav">
-            <Button
-              onMouseEnter={() => {
-                navRef.current.style.display = "block";
-              }}
-            >
-              show nav
-            </Button>
+        <div className="header__web">
+          <div className="header_menu">
+            <img
+              src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+              alt="logo"
+              width={30}
+            />
+            <h4>PHẦN MỀM QUẢN LÝ KIỂM SOÁT GIẾT MỔ VÀ KIỂM DỊCH</h4>
+            <div className="btn-nav">
+              <Button
+                onMouseEnter={() => {
+                  navRef.current.style.display = "block";
+                }}
+              >
+                show nav
+              </Button>
+            </div>
           </div>
-        </div>
-        <nav className="nav-bar-top" ref={navRef}>
-          <ul>
-            <li>
+          <ul className="nav-bar-top" ref={navRef}>
+            <li onClick={handleClicElements1}>
               <div className="dropdown-menu">
-                <Button type="link">Kiểm dịch</Button>
+                <Button type="link" className="dropdown-menu__btn">
+                  Kiểm dịch
+                  <span
+                    className={`icon ${
+                      iconRotate1 ? "iconRotate--90" : "iconRotate-0"
+                    }`}
+                  >
+                    <i className="fa-solid fa-angle-left"></i>
+                  </span>
+                </Button>
                 <div className="dropdown-content">
-                  <ul>
+                  <ul
+                    className={`dropdown-content-1 ${
+                      showItems1 ? "showItems" : ""
+                    }`}
+                  >
                     <li>
                       <Link to={quarantineEndpoints.home}>
                         Báo cáo kiểm dịch
@@ -58,11 +100,24 @@ const Header = () => {
                 </div>
               </div>
             </li>
-            <li>
+            <li onClick={handleClicElements2} className="mb20">
               <div className="dropdown-menu">
-                <Button type="link">Giết mổ</Button>
+                <Button type="link" className="dropdown-menu__btn">
+                  Giết mổ
+                  <span
+                    className={`icon ${
+                      iconRotate2 ? "iconRotate--90" : "iconRotate-0"
+                    }`}
+                  >
+                    <i className="fa-solid fa-angle-left"></i>
+                  </span>
+                </Button>
                 <div className="dropdown-content">
-                  <ul>
+                  <ul
+                    className={`dropdown-content-1 ${
+                      showItems2 ? "showItems" : ""
+                    }`}
+                  >
                     <li>
                       <Link to={abattoirEndpoints.home}>Báo cáo giết mổ</Link>
                     </li>
@@ -76,11 +131,24 @@ const Header = () => {
                 </div>
               </div>
             </li>
-            <li>
+            <li onClick={handleClicElements3}>
               <div className="dropdown-menu">
-                <Button type="link">Quản trị admin</Button>
+                <Button type="link" className="dropdown-menu__btn">
+                  Quản trị admin
+                  <span
+                    className={`icon ${
+                      iconRotate3 ? "iconRotate--90" : "iconRotate-0"
+                    }`}
+                  >
+                    <i className="fa-solid fa-angle-left"></i>
+                  </span>
+                </Button>
                 <div className="dropdown-content">
-                  <ul>
+                  <ul
+                    className={`dropdown-content-1 ${
+                      showItems3 ? "showItems" : ""
+                    }`}
+                  >
                     <li>
                       <Link to={staffEndpoints.home}>Quản lý nhân viên</Link>
                     </li>
@@ -103,7 +171,8 @@ const Header = () => {
               </div>
             </li>
           </ul>
-        </nav>
+          <div id="menu-responsive" className="fa-solid fa-bars"></div>
+        </div>
       </PageHeader>
     </>
   );
